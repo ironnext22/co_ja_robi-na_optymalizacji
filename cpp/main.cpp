9543fd1de6a58f68537cf9facb7212b4d9b40676
ironnext22 <97704600+ironnext22@users.noreply.github.com>
2023-10-12 15:00:16 +0200

    init

---
+++ cpp/main.cpp
@@ -0,0 +1,86 @@
+#include"src/opt_alg.h"
+
+void lab0();
+void lab1();
+void lab2();
+void lab3();
+void lab4();
+void lab5();
+void lab6();
+
+int main()
+{
+	try
+	{
+		lab0();
+	}
+	catch (string EX_INFO)
+	{
+		cerr << "ERROR:\n";
+		cerr << EX_INFO << endl << endl;
+	}
+	return 0;
+}
+
+void lab0()
+{
+	//Funkcja testowa
+	double epsilon = 1e-2;
+	int Nmax = 10000;
+	matrix lb(2, 1, -5), ub(2, 1, 5), a(2, 1);
+	solution opt;
+	a(0) = -1;
+	a(1) = 2;
+	opt = MC(ff0T, 2, lb, ub, epsilon, Nmax, a);
+	cout << opt << endl << endl;
+	solution::clear_calls();
+
+	//Wahadlo
+	Nmax = 1000;
+	epsilon = 1e-2;
+	lb = 0;
+	ub = 5;
+	double teta_opt = 1;
+	opt = MC(ff0R, 1, lb, ub, epsilon, Nmax, teta_opt);
+	cout << opt << endl << endl;
+	solution::clear_calls();
+
+	//Zapis symulacji do pliku csv
+	matrix Y0 = matrix(2, 1), MT = matrix(2, new double[2]{ m2d(opt.x),0.5 });
+	matrix* Y = solve_ode(df0, 0, 0.1, 10, Y0, NAN, MT);
+	ofstream Sout("../../py/symulacja_lab0.csv");
+	Sout << hcat(Y[0], Y[1]);
+	Sout.close();
+	Y[0].~matrix();
+	Y[1].~matrix();
+}
+
+void lab1()
+{
+
+}
+
+void lab2()
+{
+
+}
+
+void lab3()
+{
+
+}
+
+void lab4()
+{
+
+}
+
+void lab5()
+{
+
+}
+
+void lab6()
+{
+
+}
