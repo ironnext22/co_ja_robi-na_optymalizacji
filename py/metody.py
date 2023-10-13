90a9055d9e338ce107165e7938b9fd174504ad55
NightnHawk <116789188+NightnHawk@users.noreply.github.com>
2023-10-13 17:03:17 +0200

    Update metody.py
    Metoda ekspansji

--- py/metody.py
+++ py/metody.py
@@ -1,44 +1,20 @@
-import numpy as np
-
-
-# def ekspansji(x0,f,d,alpha,N):
-#     i=0
-#     x = [x0, x0 + d]
-#     if f(x[1]) == f(x[0]):
-#         return x[0],x[1]
-#     if f(x[1]) > f(x[0]):
-#         d = -d
-#         x[1] = x[0] + d
-#         if f(x[1]) >= f(x[0]):
-#             return x[1], x[0] - d
-#     for i in N:
-#         i = i+1
-#         #x[i+1] = x0 + (alpha**i)*d
-#         x.append(x0 + (alpha**i)*d)
-#         if f(x[1]) <= f(x[i+1]):
-#             break
-#     if d>0:
-#         return x[i - 1],x[i + 1]
-#     return x[i + 1],x[i - 1]
-
-def ekspansji(x0, f, d, alpha, N):
-    x = [x0, x0 + d]
-    i=0
-    if f(x[1]) == f(x[0]):
-        return x[0], x[1]
-
-    if f(x[1]) > f(x[0]):
+def ekspansja(x0: float, d: float, alpha: float, nmax: int, func):
+    i = 0
+    x_list = [x0, x0 + d]
+    if func(x_list[0]) == func(x_list[1]):
+         return x_list[0], x_list[1]
+    if func(x_list[1]) > func(x_list[0]):
         d = -d
-        x[1] = x[0] + d
-        if f(x[1]) >= f(x[0]):
-            return x[1], x[0] - d
-
-    for i in range(N):
-        x.append(x0 + (alpha**i) * d)
-        if f(x[i]) <= f(x[i + 1]):
+        x_list[1] = x_list[0] + d
+        if func(x_list[1]) >= func(x_list[0]):
+            return x_list[1], x_list[0] - d
+    while True:
+        if i > nmax:
+            raise ValueError("Nmax has been exceeded")
+        i += 1
+        x_list.append(x_list[0] + alpha**i * d)
+        if func(x_list[i]) <= func(x_list[i + 1]):
             break
-
-    if d > 0:
-        return x[i - 1], x[i+1]
-
-    return x[i+1], x[i - 1]
+    if(d > 0):
+        return x_list[i - 1], x_list[i + 1]
+    return x_list[i + 1], x_list[i - 1]
