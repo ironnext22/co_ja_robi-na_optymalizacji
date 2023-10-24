ff325828ee5f859252d85bbe93406f8b71329703
ironnext22 <97704600+ironnext22@users.noreply.github.com>
2023-10-24 15:36:20 +0200

    1

--- py/fun.py
+++ py/fun.py
@@ -1,3 +1,16 @@
 from math import *
-def f1(x):
-    return -cos(0.1*x)*exp(-((0.1*x-2*pi)**2))+0.002*(0.1*x)**2
+
+class funclass:
+
+    def __init__(self):
+        self.calls = 0
+
+    def f1(self,x):
+        return -cos(0.1 * x) * exp(-((0.1 * x - 2 * pi) ** 2)) + 0.002 * (0.1 * x) ** 2
+        self.calls = self.calls + 1
+
+
+    def fcalls(self):
+        return self.calls
+    def zerocalls(self):
+        self.calls = 0
